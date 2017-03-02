bd29e5f6c2402fc31a7311ef5e24620517766d43
zaneta989 <zaneta989@gmail.com>
2017-03-02 20:25:24 +0100

    dodanie metody show w articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,9 @@ class ArticlesController < ApplicationController
 
   def new
   end
-
+  def show
+    @article = Article.find(params[:id])
+  end
   def create
     @article = Article.new(article_params)
 
