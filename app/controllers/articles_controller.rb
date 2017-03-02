a378e5db5cf4d6ac3dea428426c19266c6a15416
zaneta989 <zaneta989@gmail.com>
2017-03-02 20:53:31 +0100

    sprawdzanie czy tytul artykulu ma odpowiednia dlugosc

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,8 +11,11 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
   private
   def article_params
