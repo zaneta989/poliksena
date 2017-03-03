dc396bc73849bcdfacba2cb93e544d255bdb2559
zaneta989 <zaneta989@gmail.com>
2017-03-03 14:04:07 +0100

    nowy model Comment

--- db/migrate/20170303130017_create_comments.rb
+++ db/migrate/20170303130017_create_comments.rb
@@ -1,9 +1,10 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
-      t.commenter :string
-      t.body :text
-      t.article :references, foreign_key: true
+      t.string :commenter
+      t.text :body
+      t.references :article, foreign_key: true
+
       t.timestamps
     end
   end
