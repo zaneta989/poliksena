75ab728c5bce9ddfa48e96859f4759b88ce0207e
zaneta989 <zaneta989@gmail.com>
2017-03-03 13:40:34 +0100

    dodanie foreign key w bazie danych

--- db/migrate/20170303123355_create_comments.rb
+++ db/migrate/20170303123355_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration[5.0]
     create_table :comments do |t|
       t.commenter :string
       t.body :text
-      t.article :references
+      t.article :references, foreign_key: true
       t.timestamps
     end
   end
