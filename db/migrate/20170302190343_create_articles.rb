7377954be87ee718695a3595a4323dc151606e40
zaneta989 <zaneta989@gmail.com>
2017-03-15 14:16:44 +0100

    treaser posta + data posta

--- db/migrate/20170302190343_create_articles.rb
+++ db/migrate/20170302190343_create_articles.rb
@@ -3,6 +3,7 @@ class CreateArticles < ActiveRecord::Migration[5.0]
     create_table :articles do |t|
       t.string :title
       t.text :text
+      t.date :updated_at
       t.timestamps
     end
   end
