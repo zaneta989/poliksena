b0f39177c5a9b52b8470fc43e8a3b6a5e17ad4dd
zawias2704 <pawel.zawislak@hotmail.com>
2017-03-26 22:32:54 +0200

    poprawienie routingu

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
     resources :comments
   end
   resources :tags
+  resources :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -54,10 +54,12 @@ ActiveRecord::Schema.define(version: 20170326131650) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
+    t.string   "username"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "encrypted_password"
+    t.string   "salt"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
 end
