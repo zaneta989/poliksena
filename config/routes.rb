c2329c59c7f22dae8cb2d41680f9e17867e22367
zawias2704 <pawel.zawislak@hotmail.com>
2017-03-29 21:24:59 +0200

    linki do rejestracji i logowania

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'welcome#index'
-  get '/' => 'welcome#index'
+  root to: 'start#index'
+  get '/' => 'start#index'
   resources :articles do
     resources :comments
   end
   resources :tags
-  resources :users
 end
