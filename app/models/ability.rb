ca78ef50c18289e276be7026087b2c8679f0b5cc
zawias2704 <pawel.zawislak@hotmail.com>
2017-04-06 23:46:25 +0200

    zmiana linkow w widokach, klasa Ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,16 +2,10 @@ class Ability
     include CanCan::Ability
 
     def initialize(user)
-      # Define abilities for the passed in user here. For example:
-
       user ||= User.new # guest user (not logged in)
-      if user.superadmin_role?
+      if user.admin?
         can :manage, :all
-        can :access, :rails_admin       # only allow admin users to access Rails Admin
-        can :dashboard                  # allow access to dashboard
-      end
-      if user.supervisor_role?
-        can :manage, User
+        can :access, :rails_admin
       end
     end
 end
